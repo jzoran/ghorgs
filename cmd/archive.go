652abe5241e2d8f38e7679df5fe502fb2cdcfc1c
Zoran Jovanovic <zoran.jovanovic@sony.com>
2020-03-20 14:37:21 +0100

    archive: document and validate flags
    Change-Id: Ica6cf1e7afee092dfb1d9a96f61b43a536200e07

--- cmd/archive.go
+++ cmd/archive.go
@@ -8,12 +8,15 @@ import (
 	"ghorgs/cache"
 	//	"ghorgs/entities"
 	cmds "github.com/spf13/cobra"
+	"os"
+	"regexp"
+	"strings"
 )
 
 type archiveT struct {
 	n         int
 	since     string
-	names     string
+	names     []string
 	outFolder string
 	data      map[string]*cache.Table
 }
@@ -32,22 +35,43 @@ func init() {
 	archiveCmd.Flags().IntP("n",
 		"n",
 		1,
-		"Number of repositories to archive.")
+		`Number of repositories to archive.
+
+* If --n is used together with --since, then the result is:
+  "the number --n of repositories to archive --since point in time - whichever comes first."
+* If used alone, then the result is:
+  "the least active number of repositories to archive".
+
+NOTE: It will be ignored if used with --repos.
+`)
 
 	archiveCmd.Flags().StringP("since",
 		"s",
 		"",
-		"Remove repositories inactive since this date.")
+		`Remove repositories inactive since this date (YYYY-MM-DD).
+
+* If --since is used together with --n, then the result is:
+  "the number --n of repositories to archive --since point in time - whichever comes first."
+* If --since is used together with --repos, then the result is:
+  "archive the repositories from --repos list if they have been inactive --since this point in time".
+`)
 
 	archiveCmd.Flags().StringP("repos",
 		"r",
 		"",
-		"Comma separated list of repositories to archive.")
+		`Comma separated list of repositories to archive.
+
+* Name can only contain alphanumeric characters.
+* If --repos is used with --since, then the result is:
+  "archive the repositories from --repos list if they have been inactive --since this point in time.
+
+NOTE: --n will be ignored if used with --repos.
+`)
 
 	archiveCmd.Flags().StringP("out",
 		"O",
-		"",
-		"Output folder where archives of repositories are recorded..")
+		".",
+		"Output folder where archives of repositories are recorded.")
 
 	rootCmd.AddCommand(archiveCmd)
 
@@ -58,6 +82,64 @@ func (a *archiveT) addCache(c map[string]*cache.Table) {
 }
 
 func (a *archiveT) validateArgs(c *cmds.Command, args []string) error {
+	var err error
+
+	// verify that the number of repos is a positive integer
+	a.n, err = c.Flags().GetInt("n")
+	if err != nil {
+		panic(err)
+	}
+
+	if a.n <= 0 {
+		return fmt.Errorf("Insert --n greater than 0.")
+	}
+
+	// verify that the date is in format YYYY-MM-DD, starting from 1900-01-01
+	a.since, err = c.Flags().GetString("since")
+	if err != nil {
+		panic(err)
+	}
+	if a.since != "" {
+		matched, err := regexp.MatchString(`^(19|[2-9]\d)\d\d-(0?[1-9]|1[0-2])-(0?[1-9]|[12]\d|3[01])$`,
+			a.since)
+		if err != nil {
+			return err
+		}
+		if !matched {
+			return fmt.Errorf("The date --since does not match format: YYYY-MM-DD " +
+				"(starting from the 1900s)...")
+		}
+	}
+
+	// verify that repos are a comma separated list of alphanumerics and
+	// ignore number of repos to archive
+	repos, err := c.Flags().GetString("repos")
+	if err != nil {
+		panic(err)
+	}
+	if repos != "" {
+		// matched, err := regexp.MatchString(`^[[:alnum:]]+(\,[[:alnum:]]+)*$`, repos)
+		matched, err := regexp.MatchString(`^[\.|\-|\_|[:alnum:]]+(\,[\.|\-|\_|[:alnum:]]+)*$`, repos)
+		if err != nil {
+			return err
+		}
+		if !matched {
+			return fmt.Errorf("--repos can only contain a comma separated list of repository names written in ascii alpha-numeric characters.")
+		}
+
+		a.names = strings.Split(repos, ",")
+		a.n = 0
+	}
+
+	// verify path
+	a.outFolder, err = c.Flags().GetString("out")
+	if err != nil {
+		panic(err)
+	}
+	if _, err := os.Stat(a.outFolder); os.IsNotExist(err) {
+		return err
+	}
+
 	return nil
 }
 
